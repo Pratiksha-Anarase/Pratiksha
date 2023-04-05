310e2488148828354adc060cb8797b6385f61cb5
Pratiksha <pratikshaanarase2272gmail.com>
2023-04-05 16:13:31 +0530

    commit

---
+++ random.java
@@ -0,0 +1,8 @@
+public class random {
+
+  public static void main(String[] args) {
+    for(int i=0; i < 10 ; i++){
+      System.out.println("Random Number ["+ (i+1) + "] : " + (int)(Math.random()*100));
+    }
+  }
+}       
